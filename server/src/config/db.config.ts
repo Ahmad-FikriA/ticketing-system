18edb4eb99a09378ba60564d01239dbcec256276
Ahmad-FikriA <fikria0605@gmail.com>
2026-01-06 12:59:12 +0700

    Add initial project structure with server configuration, Prisma setup, and environment management

---
+++ server/src/config/db.config.ts
@@ -0,0 +1,4 @@
+import dotenv from "dotenv";
+
+export const port = Number(process.env.PORT);
+export const access_jwt_secret = process.env.ACCESS_JWT_SECRET
---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import { port } from "./config/db.config.ts";
+
+const app = express();
+
+app.use(helmet());
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+})
+);
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// ! API HERE
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
